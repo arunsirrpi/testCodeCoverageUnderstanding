257c917f102c815fc7ca86db9026f92867e3600e
Arun Sirrpi <sinthanaisirrpi.arun@abc.net.au>
2022-08-02 15:39:38 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TestCoverageUnderstanding
+
+A description of this package.
---
+++ Sources/TestCoverageUnderstanding/TestCoverageUnderstanding.swift
@@ -0,0 +1,6 @@
+public struct TestCoverageUnderstanding {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
